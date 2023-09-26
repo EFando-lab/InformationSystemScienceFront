0f3f527e6d5dc4e7f2f85fd362ed0143705fc62e
InvestigatorI <tempemailforitlabgithub@chitthi.in>
2023-09-26 13:00:05 +0300

    first commit 1

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { first } from 'rxjs';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.less']
+})
+export class LoginComponent implements OnInit {
+  form!: FormGroup;
+  loading = false;
+  submitted = false;
+
+  constructor(
+      private formBuilder: FormBuilder,
+      private route: ActivatedRoute,
+      private router: Router,
+      private authService: AuthService,
+      // private alertService: AlertService
+  ) { }
+
+  ngOnInit() {
+      this.form = this.formBuilder.group({
+          username: ['', Validators.required],
+          password: ['', Validators.required]
+      });
+  }
+
+  get f() { return this.form.controls; }
+
+  onSubmit() {
+      this.submitted = true;
+
+      // reset alerts on submit
+      // this.alertService.clear();
+
+      // stop here if form is invalid
+      if (this.form.invalid) {
+          return;
+      }
+
+      this.loading = true;
+      this.authService.login({ username: this.f['username'].value, password: this.f['password'].value})
+          .pipe(first())
+          .subscribe({
+              next: () => {
+                  // get return url from query parameters or default to home page
+                  const returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+                  this.router.navigateByUrl(returnUrl);
+              },
+              error: error => {
+                  console.log(error);
+                  this.loading = false;
+              }
+          });
+  }
+}
